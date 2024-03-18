aa69044d28b94dc8fa38ffafbff5ce1049013df9
AlphaGao <498662779@qq.com>
2024-03-18 21:31:06 +0800

    169: majority elements

--- 169_majority_element/main.go
+++ 169_majority_element/main.go
@@ -22,3 +22,20 @@ func majorityElementWithSort(nums []int) int {
 	sort.Ints(nums)
 	return nums[len(nums)/2]
 }
+
+func majorityElement(nums []int) int {
+	much, time := nums[0], 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == much {
+			time++
+		} else {
+			if time == 0 {
+				much = nums[i]
+				time++
+			} else {
+				time--
+			}
+		}
+	}
+	return much
+}
