1f7d5001de6702daab050c50100dbc04da1d7066
yanqiang.gao <yanqiang.gao@ucloud.cn>
2024-03-27 15:44:20 +0800

    012: int to roman with mod

--- 012_int_to_roman/main.go
+++ 012_int_to_roman/main.go
@@ -22,9 +22,22 @@ func intToRoman(num int) string {
 	return str
 }
 
+var (
+	thousands = []string{"", "M", "MM", "MMM"}
+	hundreds  = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	tens      = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+	ones      = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+)
+
+func mod(num int) string {
+
+	return thousands[num/1000] + hundreds[num%1000/100] + tens[num%100/10] + ones[num%10]
+
+}
+
 func main() {
 	ints := []int{3, 9, 58, 1994}
 	for i := range ints {
-		fmt.Println(intToRoman(ints[i]))
+		fmt.Println(mod(ints[i]))
 	}
 }
